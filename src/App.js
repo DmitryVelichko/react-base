8b3d16bc124dd8b107ae9a6bc6b0e743b8ce9915
Dmitry Velichko <soleil-2005@mail.ru>
2022-07-18 19:35:35 +0300

    new commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React, { useState } from 'react';
-import './styles/App.css'
-import PostItem from './components/PostItem';
+import './styles/App.css';
+import PostList from './components/PostList.jsx';
+
 
 function App() {
-  
+  const [posts, setPosts] = useState([
+    { id: 1, title: 'JS', body: 'Description' },
+    { id: 2, title: 'JS 2', body: 'Description' },
+    { id: 3, title: 'JS 3', body: 'Description' },
+  ]);
 
-  return (
-    <div className="app">
-      <PostItem post={{id: 1, title: 'JS', body: 'Description'}}/>
-      
-    </div>
-  );
+  const [posts2, setPosts2] = useState([
+    { id: 1, title: 'Python', body: 'Description' },
+    { id: 2, title: 'Python 2', body: 'Description' },
+    { id: 3, title: 'Python 3', body: 'Description' },
+  ]);
+
+  return <div className='app'>
+    <PostList posts={posts} title='Post List JS'/>
+    <PostList posts={posts2} title='Post List Python'/>
+  </div>;
 }
 
 export default App;
