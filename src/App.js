47ad0f72c6750cca85de9b740a3fa0fc7e8f288b
Dmitry Velichko <soleil-2005@mail.ru>
2022-07-18 11:03:06 +0300

    new commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import React, {useState} from "react";
+
 function App() {
+const [value, setValue] = useState(0)
+
+function plus () {
+  setValue(value + 1)
+}
+
+function minus () {
+  setValue(value - 1)
+}
+
   return (
     <div className="App">
-     
+
+     <h1>{value}</h1>
+     <button onClick={plus}>+</button>
+     <button onClick={minus}>-</button>
     </div>
   );
 }
