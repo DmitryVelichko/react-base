11aed2b4066c64cd47a8892f321088d3be28836f
Dmitry Velichko <soleil-2005@mail.ru>
2022-07-18 23:03:23 +0300

    new commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './styles/App.css';
 import PostList from './components/PostList.jsx';
-
+import MyButton from './components/UI/button/MyButton';
 
 function App() {
   const [posts, setPosts] = useState([
@@ -10,15 +10,12 @@ function App() {
     { id: 3, title: 'JS 3', body: 'Description' },
   ]);
 
-  const [posts2, setPosts2] = useState([
-    { id: 1, title: 'Python', body: 'Description' },
-    { id: 2, title: 'Python 2', body: 'Description' },
-    { id: 3, title: 'Python 3', body: 'Description' },
-  ]);
 
   return <div className='app'>
+    <input type="text" placeholder='Post name' />
+    <input type="text" placeholder='Post description' />
+    <MyButton>Create Post</MyButton>
     <PostList posts={posts} title='Post List JS'/>
-    <PostList posts={posts2} title='Post List Python'/>
   </div>;
 }
 
