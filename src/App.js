77737fd568fdd1cfb9101d90ced6058aa59547b1
Dmitry Velichko <soleil-2005@mail.ru>
2022-07-21 21:59:25 +0300

    new commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import './styles/App.css';
 import PostList from './components/PostList.jsx';
 import MyButton from './components/UI/button/MyButton';
@@ -11,17 +11,21 @@ function App() {
     { id: 3, title: 'JS 3', body: 'Description' },
   ]);
 
-  const [title, setTitle] = useState('aaaaaaaaaaa');
+  const [title, setTitle] = useState('');
+
+  const bodyInputRef = useRef();
 
   const addNewPost = (e) => {
    e.preventDefault()
+   console.log(bodyInputRef.current.value)
   };
 
   return (
     <div className='app'>
       <MyInput value={title} onChange={e => setTitle(e.target.value)}
       type='text' placeholder='Post name' />
-      <MyInput type='text' placeholder='Post description' />
+      <input ref={bodyInputRef} type="text" />
+      {/* <MyInput ref={bodyInputRef} type='text' placeholder='Post description' /> */}
       <MyButton onClick={addNewPost}>Create Post</MyButton>
       <PostList posts={posts} title='Post List JS' />
     </div>
