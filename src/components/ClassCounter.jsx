4c06a51e7ccfb0d6b651266650fb72f855f0b6a3
Dmitry Velichko <soleil-2005@mail.ru>
2022-08-01 23:51:21 +0300

    new commit

--- src/components/ClassCounter.jsx
+++ src/components/ClassCounter.jsx
@@ -18,3 +18,16 @@ class ClassCounter extends React.Component {
     decrement() {
         this.setState({count: this.state.count - 1})
     }
+
+    render() {
+        return (
+            <div>
+                <h1>{this.state.count}</h1>
+                <button onClick={this.increment}>Increment</button>
+                <button onClick={this.decrement}>Decrement</button>
+            </div>
+        )
+    }
+}
+
+export default ClassCounter;
