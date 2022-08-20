6266d69379b483808379efe2c742a7bc580d8499
Dmitry Velichko <soleil-2005@mail.ru>
2022-08-20 13:35:12 +0300

    new commit

--- src/App.js
+++ src/App.js
@@ -17,21 +17,6 @@ function App() {
 
   const [modal, setModal] = useState(false);
 
-  const sortedPosts = useMemo(() => {
-    if (filter.sort) {
-      return [...posts].sort((a, b) =>
-        a[filter.sort].localeCompare(b[filter.sort])
-      );
-    }
-    return posts;
-  }, [filter.sort, posts]);
-
-  const sortedAndSearchedPosts = useMemo(() => {
-    return sortedPosts.filter((post) =>
-      post.title.toLowerCase().includes(filter.query.toLowerCase())
-    );
-  }, [filter.query, sortedPosts]);
-
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
     setModal(false);
