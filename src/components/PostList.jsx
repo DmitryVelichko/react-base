430648b2b6001ad11b4115f9bbc4ac501deb53be
Dmitry Velichko <soleil-2005@mail.ru>
2022-08-20 13:26:25 +0300

    new commit

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -12,7 +12,7 @@ const PostList = ({ posts, title, remove }) => {
       <h1 style={{ textAlign: 'center' }}>{title}</h1>
       <TransitionGroup>
         {posts.map((post, index) => (
-          <CSSTransition key={post.id} timeout={500} classNames='item'>
+          <CSSTransition key={post.id} timeout={500} classNames='post'>
           <PostItem
             remove={remove}
             number={index + 1}
