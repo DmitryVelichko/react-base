1ae29a7ab7e8e154e5269beafdfd78e9616c55a2
Dmitry Velichko <soleil-2005@mail.ru>
2022-09-30 23:11:27 +0400

    new commit

--- src/App.js
+++ src/App.js
@@ -7,3 +7,11 @@ const App = () => {
     <div className='App'>
       <div className="post">
         <div className="post__content">
+          <strong>1. JavaScript</strong>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
