73f3dbc449fc5e87d6b89f9eb83eb71a41feff9f
Dmitry Velichko <soleil-2005@mail.ru>
2022-09-30 23:11:22 +0400

    new commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React, {useState} from 'react'
+import Counter from './components/Counter'
+
+const App = () => {
+
+  return (
+    <div className='App'>
+      <div className="post">
+        <div className="post__content">
