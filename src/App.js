3f9040a22ba2b05d141c4a7850c6c12bc3a4efc1
Dmitry Velichko <soleil-2005@mail.ru>
2022-10-01 00:05:36 +0400

    new commit

--- src/App.js
+++ src/App.js
@@ -8,6 +8,12 @@ const App = () => {
       <div className="post">
         <div className="post__content">
           <strong>1. JavaScript</strong>
+          <div>
+            JavaScript - язык программирования
+          </div>
+        </div>
+        <div className="post__btns">
+          <button>Удалить</button>
         </div>
       </div>
     </div>
