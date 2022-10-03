b070edc78d997b13d68604a77a2e3e8c6ea022dd
Dmitry Velichko <soleil-2005@mail.ru>
2022-10-03 18:13:37 +0400

    new commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react'
-import PostItem from './components/PostItem'
+import PostList from './components/PostList'
 import './styles/App.css'
 
 const App = () => {
@@ -10,9 +10,17 @@ const App = () => {
     {id: 3, title: 'JavaScript 3', body: 'Description'},
   ])
 
+  const [posts2, setPosts2] = useState([
+    {id: 1, title: 'Python', body: 'Description'},
+    {id: 2, title: 'Python 2', body: 'Description'},
+    {id: 3, title: 'Python 3', body: 'Description'},
+  ])
+
   return (
     <div className='App'>
-      
+      <PostList posts={posts} title='Список постов 1'/>
+      <PostList posts={posts2} title='Список постов 2'/>
+
     </div>
   )
 }
