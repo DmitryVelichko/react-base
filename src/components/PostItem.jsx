0b6b49e7122455fa785a42f65f3d369abbb25144
Dmitry Velichko <soleil-2005@mail.ru>
2022-10-05 17:48:55 +0400

    new commit

--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const PostItem = ({post}) => {
+const PostItem = ({post, number}) => {
   return (
     <div className="post">
         <div className="post__content">
-          <strong>{`${post.id}. ${post.title}`}</strong>
+          <strong>{`${number + 1}. ${post.title}`}</strong>
           <div>
             {post.body}
           </div>
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -5,8 +5,8 @@ const PostList = ({posts, title}) => {
   return (
     <div>
       <h1 style={{ textAlign: 'center' }}>{title}</h1>
-      {posts.map((post) => (
-        <PostItem post={post} />
+      {posts.map((post, index) => (
+        <PostItem number={index} post={post} key={post.id} />
       ))}
     </div>
   );
