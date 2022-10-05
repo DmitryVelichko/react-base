be3d90bac5af33160211af2315f5235687aca4fc
Dmitry Velichko <soleil-2005@mail.ru>
2022-10-05 22:45:02 +0400

    new commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, {useState, useRef} from 'react'
+import PostForm from './components/PostForm'
 import PostList from './components/PostList'
-import MyButton from './components/UI/button/MyButton'
-import MyInput from './components/UI/input/MyInput'
 import './styles/App.css'
 
 const App = () => {
@@ -12,24 +11,12 @@ const App = () => {
     {id: 3, title: 'JavaScript 3', body: 'Description'},
   ])
 
-  const [post, setPost] = useState({title: '', body: ''})
+  
 
-  const addNewPost = (e) => {
-    e.preventDefault()
-
-    setPosts([...posts, {...post, id: Date.now()}])
-
-    setPost({title: '', body: ''})
-
-  }
 
   return (
     <div className='App'>
-      <form>
-        <MyInput value={post.title} onChange={e => setPost({...post, title: e.target.value})} type="text" placeholder='Название поста'/>
-        <MyInput value={post.body} onChange={e => setPost({...post, body: e.target.value})} type="text" placeholder='Описание поста'/>
-        <MyButton onClick={addNewPost}>Создать пост</MyButton>
-      </form>
+      <PostForm />
       <PostList posts={posts} title='Посты про JS'/>
     </div>
   )
