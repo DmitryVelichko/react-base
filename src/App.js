444948c596de07887efa4684ed1cf810a3037e83
Dmitry Velichko <soleil-2005@mail.ru>
2022-10-07 14:11:44 +0400

    new commit

--- src/App.js
+++ src/App.js
@@ -9,13 +9,17 @@ import { usePosts } from './hooks/usePosts';
 import PostService from './API/PostService';
 import Loader from './components/UI/Loader/Loader';
 import axios from 'axios';
+import { useFetching } from './hooks/useFetching';
 
 const App = () => {
   const [posts, setPosts] = useState([]);
   const [filter, setFilter] = useState({ sort: '', query: '' });
   const [modal, setModal] = useState(false);
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
-  const [isPostsLoading, setIsPostsLoading] = useState(false);
+  const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
+    const posts = await PostService.getAll();
+      setPosts(posts);
+  })
 
   useEffect(() => {
     fetchPosts();
@@ -25,15 +29,6 @@ const App = () => {
     setPosts([...posts, newPost]);
     setModal(false);
   };
-
-  async function fetchPosts() {
-    setIsPostsLoading(true);
-    setTimeout(async () => {
-      const posts = await PostService.getAll();
-      setPosts(posts);
-      setIsPostsLoading(false);
-    }, 1000);
-  }
 
   const removePost = (post) => {
     setPosts(posts.filter((p) => p.id !== post.id));
@@ -50,6 +45,9 @@ const App = () => {
 
       <hr style={{ margin: '15px 0' }} />
       <PostFilter filter={filter} setFilter={setFilter} />
+      {postError && 
+        <h1>Произошла ошибка "{postError}"</h1>
+      }
       {isPostsLoading ? (
         <div style={{ display: 'flex', justifyContent: 'center', marginTop: '50px' }}>
           <Loader />
